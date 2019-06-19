97942c779f74bc4f9803a9bbd86fe53818c227d1
Shardul Sapkota <sapkotashardul@gmail.com>
2019-06-19 21:23:15 +1200

    refactored project and customized chatbot icon to wizard and linked empatica framework

---
+++ Prospero/weatherBot/weatherBot-Bridging-Header.h
@@ -0,0 +1,22 @@
+//
+//  E4tester-Bridging-Header.h
+//  weatherBot
+//
+//  Created by Shardul Sapkota on 6/19/19.
+//  Copyright © 2019 Enrico Piovesan. All rights reserved.
+//
+
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <E4link/E4link.h>
+
+
+
+//#ifndef E4tester_Bridging_Header_h
+//#define E4tester_Bridging_Header_h
+//
+//
+//#endif /* E4tester_Bridging_Header_h */
--- Swift 3 Tips/FontStyles.swift
+++
@@ -1,9 +0,0 @@
-// FontStyles.swift
-extension UIFont {
-    static var bodyFont:UIFont {
-        return UIFont(name: "Helvetica", size: 14)
-    }
-    static var titleFont:UIFont {
-        return UIFont(name: "Helvetica-Bold", size: 18)
-    }
-}
